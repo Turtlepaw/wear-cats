44ff03d89bf6402ca41188bea1e8e783201dbc9b
Turtlepaw <81275769+Turtlepaw@users.noreply.github.com>
2024-02-06 23:46:14 -0500

    fix: chart preview

--- wear/src/main/java/com/turtlepaw/sleeptools/presentation/pages/history/history.kt
+++ wear/src/main/java/com/turtlepaw/sleeptools/presentation/pages/history/history.kt
@@ -6,11 +6,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -26,14 +32,30 @@ import androidx.wear.compose.material.PositionIndicator
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
 import androidx.wear.compose.material.scrollAway
+import androidx.wear.tooling.preview.devices.WearDevices
 import com.google.android.horologist.annotations.ExperimentalHorologistApi
 import com.google.android.horologist.compose.rotaryinput.rotaryWithScroll
+import com.patrykandpatrick.vico.compose.axis.horizontal.rememberBottomAxis
+import com.patrykandpatrick.vico.compose.axis.vertical.rememberStartAxis
+import com.patrykandpatrick.vico.compose.chart.Chart
+import com.patrykandpatrick.vico.compose.chart.column.columnChart
+import com.patrykandpatrick.vico.core.component.text.TextComponent
+import com.patrykandpatrick.vico.core.component.text.textComponent
+import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
+import com.patrykandpatrick.vico.core.entry.entryOf
+import com.patrykandpatrick.vico.core.legend.Legend
 import com.turtlepaw.sleeptools.presentation.Routes
 import com.turtlepaw.sleeptools.presentation.components.ItemsListWithModifier
 import com.turtlepaw.sleeptools.presentation.theme.SleepTheme
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
+import java.util.Locale
+import java.util.Map
+import kotlin.random.Random
 
+fun getRandomEntries() = List(4) { entryOf(it, Random.nextFloat() * 16f) }
 @OptIn(ExperimentalHorologistApi::class, ExperimentalWearFoundationApi::class)
 @Composable
 fun WearHistory(
@@ -47,6 +69,14 @@ fun WearHistory(
         val dayFormatter = DateTimeFormatter.ofPattern("E d")
         val timeFormatter = DateTimeFormatter.ofPattern("hh:mm a")
         val dayAndTimeFormatter = DateTimeFormatter.ofPattern("E d hh:mm a")
+        val data = history.toList().asReversed().slice(7).associate { (dateString, yValue) ->
+            LocalDate.parse(dateString) to yValue
+        }
+        val chartEntryModelProducer = ChartEntryModelProducer(
+            List(history.size){
+                entryOf(it, history.(it))
+            }
+        )
 
         Box(
             modifier = Modifier
@@ -87,6 +117,38 @@ fun WearHistory(
                     item {
                         Spacer(modifier = Modifier.padding(3.dp))
                     }
+                    item {
+                        Chart(
+                            chart = columnChart(
+                                spacing = 5.dp,
+                            ),
+                            chartModelProducer = chartEntryModelProducer,
+                            startAxis = rememberStartAxis(
+                                label = textComponent {
+                                    this.color = Color.White.toArgb()
+                                }
+                            ),
+                        bottomAxis = rememberBottomAxis(
+                            label = textComponent {
+                                this.color = Color.White.toArgb()
+                            },
+                            valueFormatter = { value, chartValues ->
+                                // get the entry at the given value index
+                                // get the local date from the entry
+                                val localDate = entry?.localDate
+                                // get the day of week from the local date
+                                val dayOfWeek = localDate?.dayOfWeek
+                                // get the first letter of the day of week
+                                val dayName = dayOfWeek?.getDisplayName(TextStyle.SHORT, Locale.getDefault())?.first()?.toString()
+                                // return the day name or an empty string if null
+                                dayName.orEmpty()
+                            }
+                        ),
+                        runInitialAnimation = false,
+                            modifier = Modifier
+                                .height(100.dp)
+                        )
+                    }
                     items(history.filterNotNull().toList().asReversed()) { time ->
                         Chip(onClick = { navigate.navigate(Routes.DELETE_HISTORY.getRoute(time.toString())) }, colors = ChipDefaults.chipColors(), border = ChipDefaults.chipBorder()) {
                             Row(
@@ -110,4 +172,29 @@ fun WearHistory(
             }
         }
     }
+}
+
+fun getRandomTime(amount: Int): MutableSet<LocalDateTime> {
+    val randomTimes = mutableSetOf<LocalDateTime>()
+
+    repeat(amount) {
+        val hour = Random.nextInt(0, 24)
+        val minute = Random.nextInt(0, 60)
+        val second = Random.nextInt(0, 60)
+
+        val randomTime = LocalDateTime.of(LocalDate.now(), LocalTime.of(hour, minute, second))
+        randomTimes.add(randomTime)
+    }
+
+    return randomTimes
+}
+
+@Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
+@Composable
+fun HistoryPreview() {
+    WearHistory(
+        navigate = NavHostController(LocalContext.current),
+        history = getRandomTime(5),
+        loading = false
+    )
 }
